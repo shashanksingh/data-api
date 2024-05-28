5b2c7958815e003516e88763e54a8c55292454c3
Shashank Singh <shashank.personal@gmail.com>
2024-05-28 19:43:29 +0100

    fixing catalog

--- src/pipeline/constants.py
+++ src/pipeline/constants.py
@@ -18,44 +18,44 @@ hashmap_of_units = {
         conversion="increaseValue1000",
     ),
     "477522a8-76fd-49da-a0b6-9867bdc39cac": Unit(name="LITRES_UNIT", label="L"),
-    "KILOLITRES_UNIT": Unit(
-        id="c0a76f6c-86a3-4333-b45c-6cf04cccddf4",
+    "c0a76f6c-86a3-4333-b45c-6cf04cccddf4": Unit(
+        name="KILOLITRES_UNIT",
         label="kL",
         convertTo="LITRES_UNIT",
         conversion="increaseValue1000",
     ),
-    "CUBIC_METERS_UNIT": Unit(
-        id="bc0e42d7-0dfe-494b-b8ea-41990f2c821c", label="Cubic meters"
+    "bc0e42d7-0dfe-494b-b8ea-41990f2c821c": Unit(
+        name="CUBIC_METERS_UNIT", label="Cubic meters"
     ),
-    "GALLONS_UNIT": Unit(
-        id="6d7a7901-ce71-49d7-b3d0-38036b69635e",
+    "6d7a7901-ce71-49d7-b3d0-38036b69635e": Unit(
+        name="GALLONS_UNIT",
         label="US Liquid Gallons",
         convertTo="LITRES_UNIT",
         conversion="usGallonsToLitres",
     ),
-    "KG_UNIT": Unit(id="7abe5fe6-f713-44c4-8f91-aa76061ed4ee", label="Kg"),
-    "TONS_UNIT": Unit(id="7ab7abca-a8f4-46be-b8f3-d408bd54d0b1", label="Tons"),
-    "TONNES_UNIT": Unit(id="cd58b882-5f78-4f5e-8d7b-ae14954db3eb", label="Tonnes"),
-    "TONNES_KM_UNIT": Unit(
-        id="301f1bf6-915b-4cca-8dbc-0225aae5d64f", label="Tonnes-km"
+    "7abe5fe6-f713-44c4-8f91-aa76061ed4ee": Unit(name="KG_UNIT", label="Kg"),
+    "7ab7abca-a8f4-46be-b8f3-d408bd54d0b1": Unit(name="TONS_UNIT", label="Tons"),
+    "cd58b882-5f78-4f5e-8d7b-ae14954db3eb": Unit(name="TONNES_UNIT", label="Tonnes"),
+    "301f1bf6-915b-4cca-8dbc-0225aae5d64f": Unit(
+        name="TONNES_KM_UNIT", label="Tonnes-km"
     ),
-    "KM_UNIT": Unit(id="c4bd40f7-c609-420a-bb15-2ce7e8aedc2b", label="Km"),
-    "PASSENGER_KM_UNIT": Unit(id="c009489c-89ab-4b04-831c-605f9c6daa7f", label="Km"),
-    "MILES_UNIT": Unit(
-        id="5275ea32-58d0-42b4-afc7-8f7f5f423dd7",
+    "c4bd40f7-c609-420a-bb15-2ce7e8aedc2b": Unit(name="KM_UNIT", label="Km"),
+    "c009489c-89ab-4b04-831c-605f9c6daa7f": Unit(name="PASSENGER_KM_UNIT", label="Km"),
+    "5275ea32-58d0-42b4-afc7-8f7f5f423dd7": Unit(
+        name="MILES_UNIT",
         label="Mi",
         convertTo="KM_UNIT",
         conversion="milesToKm",
     ),
-    "PASSENGER_MILES_UNIT": Unit(
-        id="42c7cd4f-1ce4-479b-b9c9-009f3d9317c2",
+    "42c7cd4f-1ce4-479b-b9c9-009f3d9317c2": Unit(
+        name="PASSENGER_MILES_UNIT",
         label="Mi",
         convertTo="PASSENGER_KM_UNIT",
         conversion="milesToKm",
     ),
-    "MEGALITRES_UNIT": Unit(id="da30e86d-1afa-4b73-bf94-580c64bb4acf", label="ML"),
-    "GWH_UNIT": Unit(id="d7d384f5-0152-436c-90c7-e8f696ee4baa", label="GWh"),
-    "GBP_UNIT": Unit(id="bb265c1a-23ce-4761-9a5b-47491feecb2d", label="GBP"),
-    "USD_UNIT": Unit(id="ffa91f39-ff16-4eb2-bea5-e8875aec61d0", label="USD"),
-    "KWH_NET": Unit(id="accdfdb5-9db6-41f3-a990-c426434255fc", label="KWh - net"),
+    "da30e86d-1afa-4b73-bf94-580c64bb4acf": Unit(name="MEGALITRES_UNIT", label="ML"),
+    "d7d384f5-0152-436c-90c7-e8f696ee4baa": Unit(name="GWH_UNIT", label="GWh"),
+    "bb265c1a-23ce-4761-9a5b-47491feecb2d": Unit(name="GBP_UNIT", label="GBP"),
+    "ffa91f39-ff16-4eb2-bea5-e8875aec61d0": Unit(name="USD_UNIT", label="USD"),
+    "accdfdb5-9db6-41f3-a990-c426434255fc": Unit(name="KWH_NET", label="KWh - net"),
 }
