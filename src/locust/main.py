97235890c8442f2e53aebbb79f7c31cfb5229a2c
Shashank Singh <shashank.personal@gmail.com>
2024-05-30 03:49:13 +0100

    load testing is working

--- src/locust/main.py
+++ src/locust/main.py
@@ -4,11 +4,23 @@ class MyUser(HttpUser):
     wait_time = between(5, 9)  # simulated wait time between tasks
 
     @task
-    def query_api(self):
+    def query_fuel_api(self):
         url = "/v1/query"
         payload = {
             "query": "get_all_fuel",
             "params": {"limit": "3"}
+        }
+        headers = {
+            "Content-Type": "application/json",
+            "User-Agent": "insomnia/9.2.0"
+        }
+        self.client.post(url, json=payload, headers=headers)
+
+    @task
+    def query_all_submission_api(self):
+        url = "/v1/query"
+        payload = {
+            "query": "get_all_submission",
         }
         headers = {
             "Content-Type": "application/json",
