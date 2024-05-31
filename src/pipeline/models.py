e60b0d1f4d03e1097be51ec34bfd29a36e66f272
Shashank Singh <shashank.personal@gmail.com>
2024-05-31 12:24:25 +0100

    moved it to seperate files

---
+++ src/pipeline/models.py
@@ -0,0 +1,9 @@
+@dataclass
+class Table:
+    table_name: str
+    schema_name: str
+    target_table_name: str
+    target_schema_name: str
+
+    def __hash__(self):
+        return hash(f"{self.schema_name}.{self.table_name}")
